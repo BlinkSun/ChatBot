e1e9d40f2e02a9dbfd4dcdfcf5a3b52a2a5ccd72
Damien Villeneuve <BlinkSun@users.noreply.github.com>
2014-05-05 14:32:42 -0400

    Players can enable/disable its own conversation
    Why not change the way to command it to be more ... artificial player for everyone !
    
    The command (/chatbot on/off) is to enable/disable the ChatBot for everyone, like a panic button.
    For players it's somethings like:
    "Hi Alice!";
    "Yo alice";
    "Where are you AliCe ?";
    "Hey JoeBlow, do you know Alice ?";
    ...
    When players say something with the namebot (default: alice) in the message for the first time, ChatBot ll be set to enable (on). To set it to disable (off), players need to say the namebot with the word "bye" in the same message... that make it more artificial player :p
    
    Well, instead of two commands on/off for general and players, players only need to remember to say it namebot to start and "bye" with namebot to close the conversation !

--- ChatBot.php
+++ ChatBot.php
@@ -4,7 +4,7 @@
 __PocketMine Plugin__
 name=ChatBot
 description=Feel alone in your server:P
-version=0.5
+version=0.8
 author=BlinkSun
 class=ChatBot
 apiversion=11,12
@@ -12,15 +12,17 @@ apiversion=11,12
 
 class ChatBot implements Plugin {
 	private $api;
+	private $openChatBot;
 
 	public function __construct(ServerAPI $api, $server = false) {
 		$this->api = $api;
+		$this->openChatBot = array();
 	}
 	
 	public function init() {
 		$this->api->addHandler("player.chat", array($this, "eventHandle"), 50);
 		$this->api->addHandler("player.join", array($this, "eventHandle"), 50);
-		$this->config = new Config($this->api->plugin->configPath($this)."config.yml", CONFIG_YAML, array("chatbot"=>"on","bothost"=>"http://www.pandorabots.com/pandora/talk-xml","botid"=>"f5d922d97e345aa1","botname"=>"Alice"));
+		$this->config = new Config($this->api->plugin->configPath($this)."config.yml", CONFIG_YAML, array("chatbot"=>"on","botid"=>"f5d922d97e345aa1","botname"=>"Alice"));
 		$this->api->console->register('chatbot', "[on|off] Enable or disable PandoraBot.",array($this, 'commandHandle'));
 		$this->api->console->alias("cb", "chatbot");
 	}
@@ -46,23 +48,43 @@ class ChatBot implements Plugin {
 				if($this->config->get("chatbot") == "on") {
 					$player = $data["player"];
 					$message = $data["message"];
-					$player->sendChat($message,$player);
-					$messageURL = $this->curlpost(
-						$this->config->get("bothost"),
-						array(
-							 "botid" => $this->config->get("botid"),
-							 "input" => $message,
-							 "custid" => $player->eid
-						)
-					);
-					$response = "Sorry ".$player->username.", can you repeat your last message please ?";
-					if($messageURL != false) $response = $this->XMLtoArray($messageURL);
-					$player->sendChat($response["RESULT"]["THAT"],$this->config->get("botname"));
-					return false;
+					if ((isset($this->openChatBot[$player->eid]) != false) or ((isset($this->openChatBot[$player->eid]) == false) and ($this->InStr(strtolower($message),strtolower($this->config->get("botname"))) != -1))) {
+						$this->openChatBot[$player->eid] = true;
+						$player->sendChat($message,$player);
+						$messageURL = $this->curlpost(
+							"http://www.pandorabots.com/pandora/talk-xml",
+							array(
+								 "botid" => $this->config->get("botid"),
+								 "input" => $message,
+								 "custid" => $player->username
+							)
+						);
+						$response = "Sorry ".$player->username.", can you repeat your last message please ?";
+						if($messageURL != false) $response = $this->XMLtoArray($messageURL);
+						//console(var_dump($response));
+						$player->sendChat($response["RESULT"]["THAT"],$this->config->get("botname"));
+						if(($this->InStr(strtolower($message),"bye") != -1) and ($this->InStr(strtolower($message),strtolower($this->config->get("botname"))) != -1)) {
+							unset($this->openChatBot[$player->eid]);
+							//console($this->InStr(strtolower($message),"bye") . " " . $this->InStr(strtolower($message),strtolower($this->config->get("botname"))));
+						}
+						return false;
+					}
 				}
 			break;
 		}
 	}
+	
+	public function InStr($haystack, $needle) { 
+		$pos=strpos($haystack, $needle); 
+		if ($pos !== false) 
+		{ 
+			return $pos; 
+		} 
+		else 
+		{ 
+			return -1; 
+		} 
+	} 
 	
 	public function curlpost($url, array $post = NULL, array $options = array()) {
 		$defaults = array(
@@ -153,5 +175,6 @@ class ChatBot implements Plugin {
 		return $xml_array;
 	}
 	
-	public function __destruct() {}
+	public function __destruct() {
+	}
 }
