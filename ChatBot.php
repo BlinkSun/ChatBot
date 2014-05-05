f92a293e390e44eb0d2c88a1231e348ecc05ca79
Damien Villeneuve <BlinkSun@users.noreply.github.com>
2014-05-05 15:35:03 -0400

    take off all XML functions
    XML library isnt installed with PocketMine server for LINUX
    So I use PCRE:
    
    preg_match("/<that(.*)?>(.*)?<\/that>/", $messageURL, $match);
    $response = substr($match[2], 1, -1);

--- ChatBot.php
+++ ChatBot.php
@@ -60,9 +60,11 @@ class ChatBot implements Plugin {
 							)
 						);
 						$response = "Sorry ".$player->username.", can you repeat your last message please ?";
-						if($messageURL != false) $response = $this->XMLtoArray($messageURL);
-						//console(var_dump($response));
-						$player->sendChat($response["RESULT"]["THAT"],$this->config->get("botname"));
+						if($messageURL != false) {
+							preg_match("/<that(.*)?>(.*)?<\/that>/", $messageURL, $match);
+							$response = substr($match[2], 1, -1);
+						}
+						$player->sendChat($response,$this->config->get("botname"));
 						if(($this->InStr(strtolower($message),"bye") != -1) and ($this->InStr(strtolower($message),strtolower($this->config->get("botname"))) != -1)) {
 							unset($this->openChatBot[$player->eid]);
 							//console($this->InStr(strtolower($message),"bye") . " " . $this->InStr(strtolower($message),strtolower($this->config->get("botname"))));
@@ -108,73 +110,7 @@ class ChatBot implements Plugin {
 		curl_close($ch);
 		return $result;
 	}
-	
-	public function XMLtoArray($XML) {
-		$xml_parser = xml_parser_create();
-		xml_parse_into_struct($xml_parser, $XML, $vals);
-		xml_parser_free($xml_parser);
-		$_tmp='';
-		foreach ($vals as $xml_elem) {
-			$x_tag=$xml_elem['tag'];
-			$x_level=$xml_elem['level'];
-			$x_type=$xml_elem['type'];
-			if ($x_level!=1 && $x_type == 'close') {
-				if (isset($multi_key[$x_tag][$x_level]))
-					$multi_key[$x_tag][$x_level]=1;
-				else
-					$multi_key[$x_tag][$x_level]=0;
-			}
-			if ($x_level!=1 && $x_type == 'complete') {
-				if ($_tmp==$x_tag)
-					$multi_key[$x_tag][$x_level]=1;
-				$_tmp=$x_tag;
-			}
-		}
-		foreach ($vals as $xml_elem) {
-			$x_tag=$xml_elem['tag'];
-			$x_level=$xml_elem['level'];
-			$x_type=$xml_elem['type'];
-			if ($x_type == 'open')
-				$level[$x_level] = $x_tag;
-			$start_level = 1;
-			$php_stmt = '$xml_array';
-			if ($x_type=='close' && $x_level!=1)
-				$multi_key[$x_tag][$x_level]++;
-			while ($start_level < $x_level) {
-				$php_stmt .= '[$level['.$start_level.']]';
-				if (isset($multi_key[$level[$start_level]][$start_level]) && $multi_key[$level[$start_level]][$start_level])
-					$php_stmt .= '['.($multi_key[$level[$start_level]][$start_level]-1).']';
-				$start_level++;
-			}
-			$add='';
-			if (isset($multi_key[$x_tag][$x_level]) && $multi_key[$x_tag][$x_level] && ($x_type=='open' || $x_type=='complete')) {
-				if (!isset($multi_key2[$x_tag][$x_level]))
-					$multi_key2[$x_tag][$x_level]=0;
-				else
-					$multi_key2[$x_tag][$x_level]++;
-				$add='['.$multi_key2[$x_tag][$x_level].']';
-			}
-			if (isset($xml_elem['value']) && trim($xml_elem['value'])!='' && !array_key_exists('attributes', $xml_elem)) {
-				if ($x_type == 'open')
-					$php_stmt_main=$php_stmt.'[$x_type]'.$add.'[\'content\'] = $xml_elem[\'value\'];';
-				else
-					$php_stmt_main=$php_stmt.'[$x_tag]'.$add.' = $xml_elem[\'value\'];';
-				eval($php_stmt_main);
-			}
-			if (array_key_exists('attributes', $xml_elem)) {
-				if (isset($xml_elem['value'])) {
-					$php_stmt_main=$php_stmt.'[$x_tag]'.$add.'[\'content\'] = $xml_elem[\'value\'];';
-					eval($php_stmt_main);
-				}
-				foreach ($xml_elem['attributes'] as $key=>$value) {
-					$php_stmt_att=$php_stmt.'[$x_tag]'.$add.'[$key] = $value;';
-					eval($php_stmt_att);
-				}
-			}
-		}
-		return $xml_array;
-	}
-	
+		
 	public function __destruct() {
 	}
 }
